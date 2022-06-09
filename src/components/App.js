07575332ac685e9d2438aedad217541d4810ede2
Hunter Kassow <hunterkassow@protonmail.com>
2022-06-09 13:49:56 -0400

    commit tree 66bc7f7e652e668e6b884187a63371222774af86

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import ShoppingList from "./ShoppingList";
+import Header from "./Header";
 import itemData from "../data/items";
+import { useState } from "react";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+  let [isDarkMode, setDarkMode] = useState(false)
+  const appClass = isDarkMode ? "App dark" : "App light"
+  const onDarkModeClick = () => {
+    setDarkMode(!isDarkMode)
   }
-
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+    <div className={appClass}>
+      <Header isDarkMode={isDarkMode} onDarkModeClick = {onDarkModeClick} />
       <ShoppingList items={itemData} />
     </div>
   );
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,31 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
+import Filter from "./Filter";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
-
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
-
+  let [filter, setFilter] = useState('')
+  const onCatergoryChange = (value) => setFilter(value)
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+      <Filter filter={filter} onCategoryChange={onCatergoryChange} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {items.filter(item => item.category.includes(filter)).map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
